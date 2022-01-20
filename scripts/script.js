fc36116ba7e86ba79f87c484d18c0c22463bf3c4
basv1996 <bas.vugts@gmail.com>
2022-01-20 15:07:34 +0100

    animation first scene

--- scripts/script.js
+++ scripts/script.js
@@ -4,54 +4,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let timeline = new TimelineMax()
     timeline 
-    .from('.section_1_01', 4, {
-        y: -100,
-        x: -150,
-        ease: Power3.easeInOut
-    })
-    .from('.section_1_02', 4, {
-        y: -150,
-        x: -250,
-        ease: Power3.easeInOut
-    }, '-=4') 
+    // .from('.section_1_01', 4, {
+    //     y: -100,
+    //     x: -150,
+    //     ease: Power3.easeInOut
+    // })
+    // .from('.section_1_02', 4, {
+    //     y: -150,
+    //     x: -250,
+    //     ease: Power3.easeInOut
+    // }, '-=4') 
     // heb -=4 toegevoegd als offset waarde, 
     // dit zorgt ervoor dat deze animatie 4 seconde eerder begint 
     // dan de .from animatie die ervoor is gestopt
-    .from('.section_1_03', 4, {
-        y: -80,
-        x: -100,
+    .from('.section1_text', 4, {
+        autoAlpha: 0
+    })
+    .to('.section_1_03', 4, {
+        y: -60,
+        x: 150,
+        autoAlpha: 0,
+        ease: Power3.easeInOut
+    })
+    .to('.section_1_04', 4, {
+        // y: -110,
+        x: 120,
+        autoAlpha: 0,
+        ease: Power3.easeInOut
+    })
+    .to('.section_1_05', 4, {
+       
+        x: 250,
+        autoAlpha: 0,
+        ease: Power3.easeInOut
+    })
+    .to('.section_1_06', 4, {
+      
+        x: 250,
+        autoAlpha: 0,
         ease: Power3.easeInOut
     }, '-=4')
-    .from('.section_1_04', 4, {
-        y: -100,
-        x: -150,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_05', 4, {
-        y: -80,
-        x: -200,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_06', 4, {
-        y: -100,
-        x: -350,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_07', 4, {
-        y: -50,
-        x: -150,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_08', 4, {
-        y: 50,
-        x: -350,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_09', 4, {
-        y: 100,
-        x: -200,
-        ease: Power3.easeInOut
-    }, '-=4')
+
+    
+   
 
     let Scene = new ScrollMagic.Scene({
         triggerElement: '.first-section',
